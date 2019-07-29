4e7dc3e275bb6b3df4a733fb70a8f8d476bbad8a
markkua <markkua@live.com>
2019-07-29 21:48:38 +0100

    整理文件

--- Threads.py
+++ Threads.py
@@ -33,7 +33,6 @@ class MainThread(QThread):
 			frames = self.realsense.get_aligned_frames()
 			color_image = self.realsense.get_color_image_from_frames(frames)
 			
-			
 			# 识别目标
 			
 			# 识别控制点
@@ -51,45 +50,46 @@ class MainThread(QThread):
 			result_image = color_image
 			self.img_signal.emit(result_image)
 			# print("frame rate=", 1 / (time.time() - self.frame_start_time))
-	
-class ImgShowThread(QThread):
-	"""	用于测试窗体显示的线程 """
-	
-	def __init__(self):
-		super().__init__()
-	
-	def run(self):
-		self._run_from_webcam()
-	
-	def _run_from_webcam(self):
-		cap = cv2.VideoCapture(0)
-		while True:
-			ret, image = cap.read()
-			self.img_signal.emit(image)
 
 
-class PickColorThread(QThread):
-	"""
-	单色
-	"""
-	img_signal = pyqtSignal(object)  # 输出图像的信号
-	
-	value = 0
-	
-	def __init__(self):
-		super().__init__()
-		print("thread ready")
-	
-	def __del__(self):
-		print("PickColorThread quit")
-	
-	def run(self):
-		self._run_from_webcam()
-	
-	def _run_from_webcam(self):
-		cap = cv2.VideoCapture(0)
-		while True:
-			ret, image = cap.read()
-			image = ImgProcessor.pick_color(image, self.value, 10)
-			self.img_signal.emit(image)
+# class ImgShowThread(QThread):
+# 	"""	用于测试窗体显示的线程 """
+#
+# 	def __init__(self):
+# 		super().__init__()
+#
+# 	def run(self):
+# 		self._run_from_webcam()
+#
+# 	def _run_from_webcam(self):
+# 		cap = cv2.VideoCapture(0)
+# 		while True:
+# 			ret, image = cap.read()
+# 			self.img_signal.emit(image)
+#
+#
+# class PickColorThread(QThread):
+# 	"""
+# 	单色
+# 	"""
+# 	img_signal = pyqtSignal(object)  # 输出图像的信号
+#
+# 	value = 0
+#
+# 	def __init__(self):
+# 		super().__init__()
+# 		print("thread ready")
+#
+# 	def __del__(self):
+# 		print("PickColorThread quit")
+#
+# 	def run(self):
+# 		self._run_from_webcam()
+#
+# 	def _run_from_webcam(self):
+# 		cap = cv2.VideoCapture(0)
+# 		while True:
+# 			ret, image = cap.read()
+# 			image = ImgProcessor.pick_color(image, self.value, 10)
+# 			self.img_signal.emit(image)
 
